82cd98cc86c6afe1485131117bbef9161d64e59c
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-12-11 15:53:08 -0800

    Update products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,14 +12,40 @@ const knex = require('knex')({
     },
     useNullAsDefault: true
 });
+//new route for update table in the main page
+router.get('/updates', (req, res) => {
+    knex
+        .select('*')
+        .from('prodUpdates')
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res,json({msg : `Error retrieving products: ${err}` })
+        })
+});
 
-// TODO: rewrite the routes to retrieve data from the database - Austin
+//new route for update table in the main page
+router.get('/updates2', (req, res) => {
+    knex
+        .select('*')
+        .from('prodUpdates')
+        .where('prod_amount', '>', 0)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res,json({msg : `Error retrieving products: ${err}` })
+        })
+});
 
-// // Get all
-// router.get('/', (req, res) => {res.json(employees)});
 
-// Get all products from the product table [done]
-//this query done [get all]
+//query for table with base price displayed only
+
+
+//query for table that displays cheapest prices grouped by store
+
+
 router.get('/', (req, res) => {
     console.log("Get all products")
     
@@ -41,12 +67,56 @@ router.get('/:name', (req, res) => {
     const pName = req.params.name;
     //const prodName  = req.params.p_prodName
     knex
-        .select('*')        // Retrieve all items
+        .select('s_price', 's_releasedate' )        // Retrieve all items
+        .from('products')   // Retreive from products table
+        .where('s_prodName', 'like', `%${pName}%`)
+        .then(userData => {
+            // Send products extracted from database in response
+            res.json(userData)
+        })
+        .catch(err => {
+            res.json({msg: `Error retrieving product name: ${err}`})
+        })
+});
+
+router.get('/base/:name', (req, res) => {
+    console.log("Name is " + req.params.name)   // Check back on this one later
+    const pName = req.params.name;
+    //const prodName  = req.params.p_prodName
+    knex
+        .max('p_price as max')
+        .select('p_releasedate')        // Retrieve all items
         .from('products')   // Retreive from products table
         .where('p_prodName', 'like', `%${pName}%`)
         .then(userData => {
             // Send products extracted from database in response
             res.json(userData)
+        })
+        .catch(err => {
+            res.json({msg: `Error retrieving product name: ${err}`})
+        })
+});
+
+
+// SELECT
+// locations.l_cityName, p_storeName, p_prodName, MIN(p_price)
+// FROM products, locations
+// WHERE p_prodName = 'Animal Crossing: New Horizons'	AND
+// p_storeName = 'Target' AND
+// l_cityID = p_cityNum;
+
+router.get('/:store/:prod', (req, res) => {
+    const store = req.params.store;
+    const prod = req.params.prod;
+
+    knex    
+        .select('p_storeName', 'p_storeNum')
+        .min('p_price as min')
+        .from('products')
+        .where('p_prodName', 'LIKE', `${prod}`)
+        .andWhere('p_storeName', 'LIKE', `${store}`)
+        .then(data => {
+            res.json(data)
         })
         .catch(err => {
             res.json({msg: `Error retrieving product name: ${err}`})
